bcbb7b44a224b80e3de84463e36cf6bea272d9ac
Matthew K <97872597+superdude311@users.noreply.github.com>
2025-05-28 22:08:46 -0700

    fixed address writing and pin init

--- 6502_flashprog.c
+++ 6502_flashprog.c
@@ -16,7 +16,7 @@
 #define D5 PA12
 #define D6 PA13
 #define D7 PA14
-const int databus[8] = {D0, D1, D2, D3, D4, D5, D6, D7}; // Array for data bus pins
+// const int databus[8] = {D0, D1, D2, D3, D4, D5, D6, D7}; // Array for data bus pins
 
 #define A0 PA15 // Address bus pins
 #define A1 PA16
@@ -27,17 +27,16 @@ const int databus[8] = {D0, D1, D2, D3, D4, D5, D6, D7}; // Array for data bus p
 #define A6 PA21 
 #define A7 PA22 
 #define A8 PA23 
-#define A9 PA24 
-#define A10 PA0
-#define A11 PA1
-#define A12 PA2
-#define A13 PA3
-#define A14 PA4
-#define A15 PA5 
-const int addrbus[16] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15}; // Array for address bus pins
+#define A9 PA0
+#define A10 PA1
+#define A11 PA2
+#define A12 PA3
+#define A13 PA4
+#define A14 PA5
+#define A15 PA6 
+// const int addrbus[16] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15}; // Array for address bus pins
 
 void pin_init();
-void set_pin_output(int pin);
 int read_databus();
 void write_addressbus(int data);
 
@@ -49,25 +48,15 @@ int main() {
 
 
     funDigitalWrite(BUS_ENABLE, FUN_LOW); // set BUS_ENABLE to low
-    for (int i = 0; i < 8; i++) {
-        set_pin_output(databus[i]); // Set data bus pins as outputs
-    }
-    for (int i = 0; i < 16; i++) {
-        set_pin_output(addrbus[i]); // Set address bus pins as outputs
-    }
     int testaddr = 0x1234; // test address to write
     write_addressbus(testaddr); // write  the address to the address bus
 }
 
-void pin_init() { // initializes extended databus GPIO pins
-    for (int i = 16; i <= 24; i++) {
-        int port_a_ext = GPIOA->CFGXR;
-        port_a_ext |= 1 << ((i - 16) * 4); // Set PA16 to PA24 as outputs
-    }
-}
+void pin_init() { // initializes addrbus GPIO pins as outputs
+    GPIOA->CFGLR |= 0x01111111;    // Use PA0 to PA6 to outputs
+    GPIOA->CFGHR |= 0x10000000;    // Use PA15 as output
+    GPIOA->CFGXR |= 0x11111111;    // Use PA16 to PA23 as outputs
 
-void set_pin_output(int pin){
-    funPinMode(pin, FUN_OUTPUT); // Set the specified pin as output
 }
 
 int read_databus() { // reads the data bus (PC15, PA8 to PA14)
@@ -82,6 +71,6 @@ int read_databus() { // reads the data bus (PC15, PA8 to PA14)
 }
 
 void write_addressbus(int address) {
-    int flipped_addr = ((address & 0x1F) << 11) | ((address & 0xFFE0) << 15); // flip and shift address bits to match wiring
-    GPIOA->OUTDR = (GPIOA->OUTDR & 0xF803FF) | flipped_addr; // set only the address bits in the GPIOA output data register
+    int flipped_addr = ((address & 0x1FF) << 15) | ((address & 0xFE00) >> 9); // flip and shift address bits to match wiring
+    GPIOA->OUTDR = (GPIOA->OUTDR & 0xFF807F) | flipped_addr; // set only the address bits in the GPIOA output data register
 }
